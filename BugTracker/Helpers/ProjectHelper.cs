95b0256c5827b5e2812f2c7f5446e0ed91a7da63
Ryan Fleming <ryan@ponderingcode.com>
2016-08-12 15:25:49 -0400

    added helper classes for user roles and projects

---
+++ BugTracker/Helpers/ProjectHelper.cs
@@ -0,0 +1,20 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Helpers
+{
+    public class ProjectHelper
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        //public bool IsUSerOnProject(string userId, int projectId)
+        //{
+        //    var project = db.Projects.Find(projectId);
+        //    var flag = project.Users.Any(u => u.Id == userId);
+        //    return flag;
+        //}
+    }
+}
---
+++ BugTracker/Helpers/UserRolesHelper.cs
@@ -0,0 +1,81 @@
+﻿using BugTracker.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Helpers
+{
+    public class UserRolesHelper
+    {
+        private UserManager<ApplicationUser> manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public bool IsUserInRole(string userId, string roleName)
+        {
+            return manager.IsInRole(userId, roleName);
+        }
+
+        public ICollection<string> ListUserRoles(string userId)
+        {
+            return manager.GetRoles(userId);
+        }
+
+        public bool AddUserToRole(string userId, string roleName)
+        {
+            var result = manager.AddToRole(userId, roleName);
+            return result.Succeeded;
+        }
+
+        public bool AddUserToRoles(string userId, string[] roleNames)
+        {
+            var result = manager.AddToRoles(userId, roleNames);
+            return result.Succeeded;
+        }
+
+        public bool RemoveUserFromRole(string userId, string roleName)
+        {
+            var result = manager.RemoveFromRole(userId, roleName);
+            return result.Succeeded;
+        }
+
+        public bool RemoveUserFromRoles(string userId, string[] roleNames)
+        {
+            var result = manager.RemoveFromRoles(userId, roleNames);
+            return result.Succeeded;
+        }
+
+        public ICollection<ApplicationUser> UsersInRole(string roleName)
+        {
+            List<ApplicationUser> resultList = new List<ApplicationUser>();
+            List<ApplicationUser> users = manager.Users.ToList();
+
+            foreach (ApplicationUser user in users)
+            {
+                if (IsUserInRole(user.Id, roleName))
+                {
+                    resultList.Add(user);
+                }
+            }
+            return resultList;
+        }
+
+        public ICollection<ApplicationUser> UsersNotInRoles(string roleName)
+        {
+            List<ApplicationUser> resultList = new List<ApplicationUser>();
+            List<ApplicationUser> users = manager.Users.ToList();
+
+            foreach (ApplicationUser user in users)
+            {
+                if (!IsUserInRole(user.Id, roleName))
+                {
+                    resultList.Add(user);
+                }
+            }
+            return resultList;
+        }
+    }
+}
